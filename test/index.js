81f995134bc5f6ec7f40f74487d0b2928bdba36a
Adrien Risser <risseraka@gmail.com>
2015-03-06 16:22:08 -0500

    adding istanbul to test coverage; improving test coverage to 100%

--- test/index.js
+++ test/index.js
@@ -49,18 +49,70 @@ var tests = {
 
 };
 
-describe('links-absolutizer', function () {
+function buildResultWithHalLinks(key, href) {
+  var links = {};
 
-  var res = {
-    send: function () {}
+  links[key] = {
+    href: href
   };
 
-  var handler = linksAbsolutizer();
+  return {
+    _links: links
+  };
+}
+
+function buildResultWithHalEmbededdLinks(key, href) {
+  var links = {};
+
+  links[key] = {
+    href: href
+  };
+
+  return {
+    _embedded: {
+      test: {
+        _links: links
+      }
+    }
+  };
+}
+
+describe('links-absolutizer', function () {
+
+  function testHandler(url, result, send, options, done) {
+    if (!done) {
+      done = options;
+      options = {};
+    }
+
+    var req = {
+      uri: url,
+      url: url
+    };
+
+    var res = {
+      send: send,
+      result: result
+    };
+
+    var handler = linksAbsolutizer(options);
+
+    handler(req, res, done);
+  }
 
   describe('res.result', function () {
 
-    it('should return if no result is passed in res', function (done) {
-      done();
+    it('should not send anything if no result is passed in res', function (done) {
+      var result = null;
+
+      testHandler(
+        '',
+        result,
+        function () {
+          assert.equal(true, false, 'should not pass here');
+        },
+        done
+      );
     });
 
   });
@@ -75,14 +127,106 @@ describe('links-absolutizer', function () {
 
     });
 
+    describe('embed', function () {
+
+      it('should not add _embedded if options.embed is not set to true', function (done) {
+        testHandler(
+          '/',
+          buildResultWithHalEmbededdLinks('test', '/b'),
+          function (json) {
+            assert.equal(json._embedded, undefined, 'should match');
+          },
+          {
+            embed: false
+          },
+          done
+        );
+      });
+
+      it('should add _embedded if options.embed is set to true', function (done) {
+        testHandler(
+          '/',
+          buildResultWithHalEmbededdLinks('test', '/b'),
+          function (json) {
+            assert.notEqual(json._embedded, undefined, 'should not match');
+          },
+          {
+            embed: true
+          },
+          done
+        );
+      });
+
+      it('should add _embedded if embed is set in URL query even if options.embed is set to false', function (done) {
+        testHandler(
+          '/?embed',
+          buildResultWithHalEmbededdLinks('test', '/b'),
+          function (json) {
+            assert.notEqual(json._embedded, undefined, 'should not match');
+          },
+          {
+            embed: false
+          },
+          done
+        );
+      });
+
+      it('should add empty links if links aren\'t set in _embedded', function (done) {
+        testHandler(
+          '/',
+          {
+            _embedded: {
+              test: {}
+            }
+          },
+          function (json) {
+            assert.notEqual(json._embedded, undefined, 'should not match');
+          },
+          {
+            embed: true
+          },
+          done
+        );
+      });
+
+      it('should handle array  _embedded items', function (done) {
+        testHandler(
+          '/',
+          {
+            _embedded: {
+              test: [
+                buildResultWithHalLinks('test', 'a'),
+                buildResultWithHalLinks('test', 'b')
+              ]
+            }
+          },
+          function (json) {
+            assert.equal(json._embedded.test.length, 2, 'should match');
+          },
+          {
+            embed: true
+          },
+          done
+        );
+      });
+
+    });
+
   });
 
   describe('links', function () {
 
     describe('self', function () {
 
-      it('should', function (done) {
-        done();
+      it('should add a self link if not given', function (done) {
+        testHandler(
+          '/a',
+          {},
+          function (json) {
+            assert.equal(json._links.self.href, '/a', 'should match');
+          },
+          done
+        );
       });
 
     });
@@ -90,47 +234,40 @@ describe('links-absolutizer', function () {
     describe('index', function () {
 
       it('should add an index link if self is different from rootPath', function (done) {
-        var req = {
-          uri: 'http://test.com/a',
-          url: '/a'
-        };
-
-        var res = {
-          send: function (json) {
-            assert.equal(json._links.index.href, 'http://test.com/', 'should match');
+        testHandler(
+          '/a',
+          buildResultWithHalLinks('self', '/a'),
+          function (json) {
+            assert.equal(json._links.index.href, '/', 'should match');
           },
-          result: {
-            _links: {}
-          }
-        };
-
-        handler(req, res, done);
+          done
+        );
       });
 
-      it('should add a self link if not given', function (done) {
-        var req = {
-          uri: 'http://test.com/a',
-          url: '/a'
-        };
-
-        var res = {
-          send: function (json) {
-            assert.equal(json._links.index.href, 'http://test.com/', 'should match');
+      it('should not add an index link if self is same as rootPath', function (done) {
+        testHandler(
+          '/a',
+          buildResultWithHalLinks('self', '/'),
+          function (json) {
+            assert.equal(json._links.index, undefined, 'should match');
           },
-          result: {
-            _links: {}
-          }
-        };
-
-        handler(req, res, done);
+          done
+        );
       });
 
     });
 
     describe('next', function () {
 
-      it('should', function (done) {
-        done();
+      it('should remove next link if same as index', function (done) {
+        testHandler(
+          '/a',
+          buildResultWithHalLinks('next', '/'),
+          function (json) {
+            assert.equal(json._links.next, undefined, 'should match');
+          },
+          done
+        );
       });
 
     });
@@ -138,6 +275,8 @@ describe('links-absolutizer', function () {
   });
 
   describe('path handling', function () {
+
+    var handler = linksAbsolutizer();
 
     function testPath(a, b, r, done) {
       var req = {
@@ -163,7 +302,7 @@ describe('links-absolutizer', function () {
       handler(req, res, done);
     }
 
-    function testHandler(method, done) {
+    function testAllPaths(method, done) {
       async.forEach(tests[method], function (test, callback) {
         test.push(callback);
 
@@ -171,15 +310,60 @@ describe('links-absolutizer', function () {
       }, done);
     }
 
-    it('should set path', testHandler.bind(this, 'setPath'));
+    it('should set path', testAllPaths.bind(this, 'setPath'));
 
-    it('should join paths', testHandler.bind(this, 'joinPath'));
+    it('should join paths', testAllPaths.bind(this, 'joinPath'));
 
-    it('should merge paths', testHandler.bind(this, 'mergePath'));
+    it('should merge paths', testAllPaths.bind(this, 'mergePath'));
 
-    it('should set query', testHandler.bind(this, 'setQuery'));
+    it('should set query', testAllPaths.bind(this, 'setQuery'));
 
-    it('should merge queries', testHandler.bind(this, 'mergeQuery'));
+    it('should merge queries', testAllPaths.bind(this, 'mergeQuery'));
+
+    it('should resolve href with self path if not beginning with special formatting character', function (done) {
+      testHandler(
+        '/a/b/c',
+        buildResultWithHalLinks('test', 'd'),
+        function (json) {
+          assert.equal(json._links.test.href, '/a/b/d', 'should match');
+        },
+        done
+      );
+    });
+
+    it('should not do anything if no href is set in link', function (done) {
+      testHandler(
+        '/',
+        {
+          _links: {
+            test: {}
+          }
+        },
+        function (json) {
+          assert.equal(json._links.test.href, undefined, 'should match');
+        },
+        done
+      );
+    });
+
+    it('should handle array links', function (done) {
+      testHandler(
+        '/',
+        {
+          _links: {
+            test: [{
+              href: 'a'
+            }, {
+              href: 'b'
+            }]
+          }
+        },
+        function (json) {
+          assert.equal(json._links.test.length, 2, 'should match');
+        },
+        done
+      );
+    });
 
   });
 
